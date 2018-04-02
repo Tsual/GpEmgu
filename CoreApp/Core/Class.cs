53eed9ae9ac193aa3aeb93d63dca94b8c9b5aace
张良 <562974055@qq.com>
2018-04-02 18:56:04 +0800

    添加项目文件。

---
+++ CoreApp/Core/Class.cs
@@ -0,0 +1,52 @@
+﻿using Emgu.CV.CvEnum;
+using Emgu.CV.Face;
+using Emgu.CV.Util;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreApp.Core
+{
+    public class Class
+    {
+
+    }
+    class FaceRecoginzerTest
+    {
+        public static void Test()
+        {
+            using (Mat image = new Mat("testGamePic.jpg"))
+            {
+                using (Mat uimg = new Mat())
+                {
+                    using (CascadeClassifier face = new CascadeClassifier("haarcascade_frontalface_default.xml"))
+                    {
+                        CvInvoke.CvtColor(image, uimg, ColorConversion.Bgr2Gray);
+                        CvInvoke.EqualizeHist(uimg, uimg);
+
+                        Rectangle[] facesDetected = face.DetectMultiScale(uimg, 1.1, 10, new Size(20, 20));
+
+
+                        EigenFaceRecognizer efr = new EigenFaceRecognizer();
+
+                        efr.Train(new VectorOfMat(uimg), new VectorOfInt(new int[] { 1 }));
+                        var res = efr.Predict(uimg);
+
+
+                        return;
+                    }
+
+                }
+            }
+        }
+
+
+
+
+
+
+    }
+}
---
+++ EmguCabt/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmguCabt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
